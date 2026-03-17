6945c7d1250dc10297e9a83ba37bf6df015fd8a7
immathtien-dot <immathtien@gmail.com>
2026-03-17 11:59:03 -0700

    added

--- css-portfolio-grid-lightbox/js/hamburger.js
+++
@@ -1,8 +0,0 @@
-//console.log("i'm here");
-
-
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.nav-menu');
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
